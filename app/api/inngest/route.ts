d3ed6cdd6e6706989b253a4f3cc97de12f1f47f6
Ninadshirsat <ninadshirsat12@gmail.com>
2026-06-25 19:13:32 +0530

    fix: correct import and function name for reviewPullRequest in inngest route

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,10 +1,10 @@
 import { inngest } from "@/features/inngest/client";
 import { serve } from "inngest/next";
 import { processTask } from "./function";
-import { reviewPullrequest } from "@/features/reviews/server/review-pr-function";
+import { reviewPullRequest } from "@/features/reviews/server/review-pr-function";
 import { syncRepoCodebaseFunction } from "@/features/repo-sync/server/repo-sync-function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [processTask, reviewPullrequest, syncRepoCodebaseFunction],
+  functions: [processTask, reviewPullRequest, syncRepoCodebaseFunction],
 });
